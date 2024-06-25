a44a4adb13bb2d30ee49e68d9f70c9b7083b0198
Eric <73071377+Posoroko@users.noreply.github.com>
2024-06-25 12:43:16 +0200

    mes listes title only

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,7 +1,50 @@
+<script setup>
+import titleOnly from '@/components/MovieCards/title-Only.vue'
+
+const movies = [
+
+    {
+       id: 1,
+       title: "Kung Fu Panda",
+       year: 2008,
+       rating: 4,
+       comment: "Excellent film d'animation",
+       poster: "@/assets/images/kung-fu-panda.jpg"
+    },
+    {
+       id: 2,
+       title: "Star Wars 2: The Clone Wars",
+       year: 1999,
+       rating: 5,
+       comment: "Un film qui a marqué mon enfance",
+       poster: "@/assets/images/star-wars-2.jpg"
+
+    },
+    {
+       id: 3,
+       title: "Star Wars 2: The Clone Wars",
+       year: 1999,
+       rating: 3,
+       comment: "Un film qui a marqué mon enfance",
+       poster: "@/assets/images/star-wars-2.jpg"
+
+    }
+]
+</script>
+
 <template>
     <h1>Mes films</h1>
 
     <router-link to="/">
         homepage
     </router-link>
+    
+    <ul>
+        <title-Only 
+            v-for="movie in movies" :key="movie.id" 
+            :title="movie.title" :rating="movie.rating"
+        />
+    </ul>
+
 </template>
+
