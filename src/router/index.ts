cf238adacc8e92286ff65cde175ee2ad97212818
Eric <eric@posoroko.com>
2024-06-26 17:52:45 +0200

    ts repair

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,8 +4,7 @@ import Movies from '@/views/Movies.vue'
 import MovieId from '@/views/MovieId.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),  
-    base: import.meta.env.BASE_URL,
+    history: createWebHistory(),
     routes: [
         {
             path: '/',
